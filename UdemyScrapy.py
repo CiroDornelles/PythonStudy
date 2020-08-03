c18e512428e0a0465cd00b8622aa8a6defaa3a87
Thiago Maior <Thiago.Maior@innroad.com>
2020-08-03 04:36:06 -0300

    make some future work easier

--- UdemyScrapy.py
+++ UdemyScrapy.py
@@ -4,49 +4,83 @@ import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-option = Options()
-driver = webdriver.Firefox(options=option)
-driver.get("https://www.udemy.com/course/curso-python-3-completo/")
-time.sleep(20)
-driver.find_element_by_xpath("//*[@id='br']//div[2]//div[3]//div[1]//div[4]//div[3]//div//div//div//div//button").click()
-element = driver.find_element_by_xpath("//*[@id='br']//div[2]//div[3]//div[1]//div[4]//div[3]//div//div//div//div//div[3]")
-html_content = element.get_attribute('outerHTML')
-#variables
-curso = []
-item = {}
-#BeautifulSoup magic to get content of list in udemy
-soup = BeautifulSoup(html_content,features='lxml')
-tableOfValues = soup.find('div', class_=re.compile('curriculum--content--*'))
-#iterate in each atribute to get the property values
-for values in tableOfValues:
-    #Get the title of the section
-    sectionName = values.find('div', class_=re.compile('section--section-title--*'))
-    #Get all items of the section
-    sectionPreviwedList = values.findAll('div', class_=re.compile('section--lecture-title-and-description--*'))
-    #start to create a list with all values
-    item['Type'] = "Section Header"
-    item['Value'] = sectionName.next
-    #adding header of an section
-    curso.append(item.copy())
-    for sectionPreviwedItem in sectionPreviwedList:
-        item['Type'] = "Section List"
-        item['Value'] = sectionPreviwedItem.text
-        #adding items of an section
-        curso.append(item.copy())
-for item in curso:
-    if item['Type'] == 'Section Header':
-        if curso.index(item) == 0:
-            print(item['Value'])
-            print(" ")
+import json
+import os 
+
+class Trello:
+    def __init__(self):
+        self.key = os.environ['key']
+        self.token = os.environ['token']
+class Course:
+    def __init__(self,url):
+        self.url = url
+        self.name = self.getName(url)
+        self.buttonXPath = "//button[@data-purpose='show-more']"
+        self.contentXPath = "curriculum--content--PkBXH"   
+        self.sections = []
+
+    def getCourse(self, tableOfValues):
+        for value in tableOfValues:
+            sectionName = value.find('div', class_=re.compile('section--section-title--*'))
+            sectionPreviwedList = value.findAll('div', class_=re.compile('section--lecture-title-and-description--*'))
+            section = Section(sectionName.next)
+            for sectionPreviwedItem in sectionPreviwedList:
+                item = Item(sectionPreviwedItem.text)
+                section.addSectionItem(item)
+            self.addSection(section)
+        return self
+
+    def soupUdemy(self):
+        option = Options()
+        driver = webdriver.Firefox(options=option)
+        driver.get(self.url)
+        time.sleep(10)
+        self.verifyIfButtonExists(driver)
+        element = driver.find_element_by_class_name(self.contentXPath)
+        html_content = element.get_attribute('outerHTML')
+        soup = BeautifulSoup(html_content,features='lxml')
+        tableOfValues = soup.find('div', class_=re.compile('curriculum--content--*'))
+        curso = self.getCourse(tableOfValues)
+        return curso
+        
+    def getName(self, url):
+        nomeCurso = str(url)
+        nomeCurso = self.retirarPrimeiroBarra(url)
+        nomeCurso = nomeCurso.split('/')[4]
+        nomeCurso = nomeCurso.replace('-',' ')
+        return nomeCurso
+
+    def retirarPrimeiroBarra(self, url):
+        nomesembarra = str(url)
+        if nomesembarra.endswith("/"):
+            nomesembarra = nomesembarra[:-1]
+            return nomesembarra
         else:
-            print(" ")    
-            print(item['Value'])
-            print(" ")
+            return str(url)
 
-    else:
-        print(item['Value'])
+    def verifyIfButtonExists(self,driver):
+        try:
+            driver.find_element_by_xpath(self.buttonXPath).click()            
+        except:
+            print("Não possui classe button")
 
-driver.quit()
+    def addSection(self, section ):
+        self.sections.append(section)
+        
+class Section():
+    items = []
+    def __init__(self,name):
+        self.name = name
 
+    def addSectionItem(self, name):
+        item = Item(name)
+        self.items.append(item)
 
- 
+class Item():
+    def __init__(self,name):
+        self.name = name      
+
+url = "https://www.udemy.com/course/gestao-financeira/"
+curso = Course(url)
+valores = curso.soupUdemy()
+print(curso.name)
